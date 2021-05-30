f010a7953596cc871e4379db8bb1c4b4ac33f715
tonymtran36 <83971539+tonymtran36@users.noreply.github.com>
2021-05-30 02:38:10 -0400

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,23 @@
+#Numbers are primarily integers or floating pointer numbers -> 4E2
+print(2+1)
+print(2-1)
+print(2*1)
+print(3/2)
+print(7//4)
+print(7%4)
+
+print("\n")
+
+print(2**3)
+print(4**.5)
+print(2+10*10+3)
+print((2+10) * (10+3))
+
+print("\n")
+
+a = 5
+a+a
+print(a)
+
+a = a + a
+print(a)
---
+++ Variables.py
@@ -0,0 +1,22 @@
+my_dogs = 2
+print(my_dogs)
+
+my_dogs = ['Sammy', 'Frankie']
+print(my_dogs)
+
+a = 5
+print('\n', a)
+
+a+=10
+print(a)
+print(type(a))
+
+a = (1, 5)
+print(type(a))
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+
+
